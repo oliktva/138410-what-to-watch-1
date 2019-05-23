3cd7b3fdc3023bd8dbb1f17baef02067694d4338
Olga Kotova <oliktva@gmail.com>
2019-05-23 20:51:50 +0300

    fix test

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -3,9 +3,72 @@ import renderer from 'react-test-renderer';
 
 import {App} from './app';
 
+const films = [{
+  id: 1,
+  name: `Fantastic Beasts`,
+  img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+  genre: `Adventure`
+}, {
+  id: 2,
+  name: `Bohemian Rhapsody`,
+  img: `img/bohemian-rhapsody.jpg`,
+  video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+  genre: `Adventure`
+}, {
+  id: 3,
+  name: `Macbeth`,
+  img: `img/macbeth.jpg`,
+  video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+  genre: `Adventure`
+}, {
+  id: 4,
+  name: `Aviator`,
+  img: `img/aviator.jpg`,
+  video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+  genre: `Adventure`
+}, {
+  id: 5,
+  name: `We need to talk about Kevin`,
+  img: `img/we-need-to-talk-about-kevin.jpg`,
+  video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+  genre: `Adventure`
+}, {
+  id: 6,
+  name: `What We Do in the Shadows`,
+  img: `img/what-we-do-in-the-shadows.jpg`,
+  video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+  genre: `Adventure`
+}, {
+  id: 7,
+  name: `Revenant`,
+  img: `img/revenant.jpg`,
+  video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+  genre: `Adventure`
+}, {
+  id: 8,
+  name: `Johnny English`,
+  img: `img/johnny-english.jpg`,
+  video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+  genre: `Adventure`
+}, {
+  id: 9,
+  name: `Shutter Island`,
+  img: `img/shutter-island.jpg`,
+  video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+  genre: `Adventure`
+}];
+
+const genre = `All genres`;
+
 it(`renders correctly`, () => {
   const tree = renderer
-    .create(<App />)
+    .create(<App
+      films={films}
+      genre={genre}
+      loadFilms={jest.fn()}
+      setFilterByGenre={jest.fn()}
+    />)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/components/small-movie-card/small-movie-card.e2e.test.js
+++ src/components/small-movie-card/small-movie-card.e2e.test.js
@@ -8,7 +8,8 @@ const film = {
   id: 1,
   name: `Dardjeeling Limited`,
   img: `img/dardjeeling-limited.jpg`,
-  video: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
+  video: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  genre: `Drama`
 };
 
 Enzyme.configure({adapter: new Adapter()});
