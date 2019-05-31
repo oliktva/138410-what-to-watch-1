d5bd6ec20e78ed7eb8455f145eed47f9be9234e1
Olga Kotova <oliktva@gmail.com>
2019-05-31 13:50:41 +0300

    add error to state

--- src/reducers/user/user.test.js
+++ src/reducers/user/user.test.js
@@ -1,7 +1,13 @@
 import MockAdapter from "axios-mock-adapter";
 
 import configureAPI from 'src/api';
-import {Operation, ActionCreator, TOGGLE_AUTHORIZATION_REQUIRED, LOG_IN_USER} from './user';
+import {
+  Operation,
+  ActionCreator,
+  TOGGLE_AUTHORIZATION_REQUIRED,
+  LOG_IN_USER,
+  LOG_IN_USER_ERROR
+} from './user';
 
 import {user, email, password} from 'src/fixtures/user';
 
@@ -14,10 +20,18 @@ describe(`ActionCreator`, () => {
   });
 
   it(`logInUser`, () => {
-    const filmAction = ActionCreator.logInUser(user);
+    const loginAction = ActionCreator.logInUser(user);
 
-    expect(filmAction.type).toEqual(LOG_IN_USER);
-    expect(filmAction.payload).toEqual(user);
+    expect(loginAction.type).toEqual(LOG_IN_USER);
+    expect(loginAction.payload).toEqual(user);
+  });
+
+  it(`logInUserError`, () => {
+    const errorMessage = `error message`;
+    const loginAction = ActionCreator.logInUserError(errorMessage);
+
+    expect(loginAction.type).toEqual(LOG_IN_USER_ERROR);
+    expect(loginAction.payload).toEqual(errorMessage);
   });
 
   it(`log in user to app`, () => {
@@ -35,6 +49,26 @@ describe(`ActionCreator`, () => {
         expect(dispatch).toHaveBeenCalledTimes(1);
         expect(dispatch).toHaveBeenNthCalledWith(1, {
           type: LOG_IN_USER,
+          payload: [{fake: true}],
+        });
+      });
+  });
+
+  it(`log in user with error`, () => {
+    const dispatch = jest.fn();
+    const api = configureAPI(dispatch);
+    const apiMock = new MockAdapter(api);
+    const userLoginer = Operation.logInUser();
+
+    apiMock
+      .onPost(`/login`)
+      .reply(400, [{fake: true}]);
+
+    return userLoginer(dispatch, jest.fn(), api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: LOG_IN_USER_ERROR,
           payload: [{fake: true}],
         });
       });
